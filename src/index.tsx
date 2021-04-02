241e0bc60b2d355ca66e1d43bed276bff2e17dbd
sbaboshkin <sergey-baboshkin@yandex.ru>
2021-04-02 10:40:47 +0300

    fixed binary search encapsulation

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import { bubbleSort, selectionSort, insertionSort, binarySearch } from './tasks/algorithms';
 
 // let array: number[] = [200, 4, 6, 12, 5, 12, 100, 76, 1];
-// let array: string[] = ['ab', 'a', 'new', 'class', 'void', 'ww'];
-let array: boolean[] = [false, true, false, true, true];
+let array: string[] = ['ab', 'a', 'new', 'class', 'void', 'ww'];
+// let array: boolean[] = [false, true, false, true, true];
 
 // array = bubbleSort(array);
 array = selectionSort(array);
@@ -11,7 +11,7 @@ array = selectionSort(array);
 console.log(array);
 
 // const searchIndex = binarySearch(array, 5);
-// const searchIndex = binarySearch(array, 'a');
-const searchIndex = binarySearch(array, true);
+const searchIndex = binarySearch(array, 'class');
+// const searchIndex = binarySearch(array, true);
 
 console.log(searchIndex);
--- src/tasks/algorithms/4_binary_search/binarySearch.ts
+++ src/tasks/algorithms/4_binary_search/binarySearch.ts
@@ -1,12 +1,17 @@
-export function binarySearch<T>(array: Array<T>, x: T, left: number = 0, right: number = array.length - 1): number {
-  if (right >= left) {
-    const middle = Math.round(left + (right - left) / 2);
-
-    if(array[middle] === x) return middle;
-
-    if(array[middle] > x) return binarySearch(array, x, left, middle - 1);
-
-    return binarySearch(array, x, middle + 1, right);
+export function binarySearch<T>(array: Array<T>, x: T): number {
+  
+  const func = function<T>(array: Array<T>, x: T, left: number = 0, right: number = array.length - 1): number {
+    if (right >= left) {
+      const middle = Math.round(left + (right - left) / 2);
+  
+      if(array[middle] === x) return middle;
+  
+      if(array[middle] > x) return func(array, x, left, middle - 1);
+  
+      return func(array, x, middle + 1, right);
+    }
+    return -1;
   }
-  return -1;
+
+  return func(array, x, 0, array.length - 1);
 }
