018d98be97962a40c7aa6624f61fa42c93016509
sbaboshkin <sergey-baboshkin@yandex.ru>
2021-04-27 12:46:11 +0300

    fixed binary search

--- src/tasks/algorithms/4_binary_search/binarySearch.ts
+++ src/tasks/algorithms/4_binary_search/binarySearch.ts
@@ -1,17 +1,17 @@
-export function binarySearch<T>(array: Array<T>, x: T): number {
+export function binarySearch<T>(array: Array<T>, x: T, compareFunction: (a: T, b: T) => number): number {
   
-  const func = function<T>(array: Array<T>, x: T, left: number = 0, right: number = array.length - 1): number {
+  const func = function<T>(array: Array<T>, x: T, left: number = 0, right: number = array.length - 1, compareFunction: (a: T, b: T) => number): number {
     if (right >= left) {
       const middle = Math.round(left + (right - left) / 2);
   
-      if(array[middle] === x) return middle;
+      if(compareFunction(array[middle], x) === 0) return middle;
   
-      if(array[middle] > x) return func(array, x, left, middle - 1);
+      if(compareFunction(array[middle], x) > 0) return func(array, x, left, middle - 1, compareFunction);
   
-      return func(array, x, middle + 1, right);
+      return func(array, x, middle + 1, right, compareFunction);
     }
     return -1;
   }
 
-  return func(array, x, 0, array.length - 1);
+  return func(array, x, 0, array.length - 1, compareFunction);
 }
